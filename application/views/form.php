2ca533f042bbc95dba368daec6b972cca72bcb99
Willy Ganjar Saputra <willyganjarsaputra@gmail.com>
2017-11-25 15:33:17 +0700

    new

--- application/views/form.php
+++ application/views/form.php
@@ -131,7 +131,7 @@
   </div>
   <div class="form-group">
     <div class="col-sm-offset-2 col-sm-10">
-      <input type="submit" value="Simpan" class="btn btn-primary" /> <a href="/" class="btn btn-danger">Batal</a>
+      <input type="submit" value="Simpan" class="btn btn-primary" /> <a href="/ems" class="btn btn-danger">Batal</a>
     </div>
   </div>
 </form>
--- application/views/header.php
+++ application/views/header.php
@@ -34,13 +34,13 @@
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
           </button>
-          <a class="navbar-brand" href="/">EMS-SIM</a>
+          <a class="navbar-brand" href="/ems">EMS</a>
         </div>
         <div id="navbar" class="navbar-collapse collapse">
           <ul class="nav navbar-nav">
-            <li class="active"><a href="/">Home</a></li>
-            <li><a href="form">Form</a></li>
-            <li><a href="histori">Histori</a></li>	
+            <li class="active"><a href="/ems">Home</a></li>
+            <li><a href="<?php echo site_url('form');?>">Form</a></li>
+            <li><a href="<?php echo site_url('histori');?>">Histori</a></li>	
             
           </ul>
         </div><!--/.nav-collapse -->
