3ad40db5aa3d85023178638d96db2da9fbf73dd2
Willy Ganjar Saputra <willyganjarsaputra@gmail.com>
2017-12-08 10:23:15 +0700

    Tambah Info

--- application/views/info.php
+++ application/views/info.php
@@ -1,4 +1,29 @@
 <?php include("header.php"); ?>
+<ol class="breadcrumb">
+  <li><a href="/ems">Home</a></li>
+  <li class="active">Info</li>
+</ol>
+<p>LANSIA</p>
+<p>Faktanya &hellip;&hellip;</p>
+<ul>
+<li>25 dari 100 lansia mengalami sakit</li>
+<li>Penyakit terbanyak pada lanjut usia adalah Penyakit Tidak Menular (PTM) antara lain hipertensi, artris, stroke, Penyakit Paru Obstrukf Kronik (PPOK) dan Diabetes Mellitus (DM)</li>
+<li>100 orang penduduk yang masih produk!f akan menanggung 48 orang yang !dak produkif di Indonesia</li>
+<li>setiap 100 orang penduduk yang masih produktif akan menanggung 48 orang yang tidak produk!f di Indonesia.</li>
+</ul>
+<p>tips agar lansia sehat, aktif dan produktif</p>
+<div class="row">
+    <div class="col-md-6">
+        <img src="<?php echo base_url();?>dist/images/info-fisik.png" alt=""><br><br>
+        <img src="<?php echo base_url();?>dist/images/info-lakukan.png" alt=""><br><br>
+        <img src="<?php echo base_url();?>dist/images/info-makanan.png" alt="">
+    </div>
+    <div class="col-md-6">
+        <img src="<?php echo base_url();?>dist/images/info-relasi.png" alt=""><br><br>
+        <img src="<?php echo base_url();?>dist/images/info-rutin.png" alt="">
+    </div>
+</div>
+
 
 
 <?php include("footer.php"); ?>
