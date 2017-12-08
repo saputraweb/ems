9282ff5c472061d3200d9389776dc3a9a64ec496
Willy Ganjar Saputra <willyganjarsaputra@gmail.com>
2017-12-08 17:01:17 +0700

    db baru

--- ems.sql
+++ ems.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 06, 2017 at 11:10 AM
+-- Generation Time: Dec 08, 2017 at 11:00 AM
 -- Server version: 10.1.21-MariaDB
 -- PHP Version: 5.6.30
 
@@ -19,6 +19,35 @@ SET time_zone = "+00:00";
 --
 -- Database: `ems`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `data_assesst`
+--
+
+CREATE TABLE `data_assesst` (
+  `id_assesst` int(8) NOT NULL,
+  `id_pasien` int(8) NOT NULL,
+  `berbaring` int(1) NOT NULL,
+  `duduk` int(1) NOT NULL,
+  `dukberdiri` int(1) NOT NULL,
+  `berdiri` int(1) NOT NULL,
+  `berjalan` int(1) NOT NULL,
+  `meter` int(1) NOT NULL,
+  `mejangkau` int(1) NOT NULL,
+  `total` int(3) NOT NULL,
+  `created_at` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `data_assesst`
+--
+
+INSERT INTO `data_assesst` (`id_assesst`, `id_pasien`, `berbaring`, `duduk`, `dukberdiri`, `berdiri`, `berjalan`, `meter`, `mejangkau`, `total`, `created_at`) VALUES
+(2, 3, 0, 0, 2, 1, 2, 2, 0, 7, '2017-12-08 10:09:23'),
+(5, 1, 2, 2, 2, 2, 2, 2, 4, 16, '2017-12-08 10:52:39'),
+(6, 4, 0, 1, 1, 1, 1, 1, 2, 7, '2017-12-08 10:53:36');
 
 -- --------------------------------------------------------
 
@@ -39,21 +68,30 @@ CREATE TABLE `data_pasien` (
   `menarik` int(1) NOT NULL,
   `percaya` int(1) NOT NULL,
   `menciderai` int(1) NOT NULL,
-  `created_at` datetime NOT NULL
+  `hasil` varchar(8) NOT NULL,
+  `created_at` datetime NOT NULL,
+  `flagases` varchar(1) NOT NULL DEFAULT 'N'
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `data_pasien`
 --
 
-INSERT INTO `data_pasien` (`id_pasien`, `nama`, `tempat_lahir`, `tgl_lahir`, `jenkel`, `riwayat`, `malu`, `bersalah`, `merendah`, `menarik`, `percaya`, `menciderai`, `created_at`) VALUES
-(1, 'Willy Ganjar Saputra', 'garut', '2017-11-07', 'L', 'Sakit', 0, 1, 0, 1, 0, 1, '2017-11-25 09:28:21'),
-(2, 'Rina', 'Jakarta', '2017-11-21', 'P', 'sakit perut', 1, 1, 1, 1, 1, 1, '2017-11-25 09:54:08'),
-(3, 'Rina', 'garut', '2017-11-14', 'P', 'aaaa', 1, 0, 1, 0, 1, 1, '2017-11-25 10:15:23');
+INSERT INTO `data_pasien` (`id_pasien`, `nama`, `tempat_lahir`, `tgl_lahir`, `jenkel`, `riwayat`, `malu`, `bersalah`, `merendah`, `menarik`, `percaya`, `menciderai`, `hasil`, `created_at`, `flagases`) VALUES
+(1, 'Willy Ganjar Saputra', 'garut', '2017-11-07', 'L', 'Sakit', 0, 0, 0, 0, 0, 0, 'ases2', '2017-11-25 09:28:21', 'Y'),
+(2, 'Rina', 'Jakarta', '2017-11-21', 'P', 'sakit perut', 1, 1, 1, 1, 1, 1, 'satuya', '2017-11-25 09:54:08', 'N'),
+(3, 'Rina Ases', 'garut', '2017-11-14', 'P', 'aaaa', 0, 0, 0, 0, 0, 0, 'ases2', '2017-11-25 10:15:23', 'Y'),
+(4, 'Anung', 'Jakarta', '2017-12-27', 'L', 'Panas', 0, 0, 0, 0, 0, 0, 'ases2', '2017-12-08 10:53:22', 'Y');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `data_assesst`
+--
+ALTER TABLE `data_assesst`
+  ADD PRIMARY KEY (`id_assesst`);
 
 --
 -- Indexes for table `data_pasien`
@@ -66,10 +104,15 @@ ALTER TABLE `data_pasien`
 --
 
 --
+-- AUTO_INCREMENT for table `data_assesst`
+--
+ALTER TABLE `data_assesst`
+  MODIFY `id_assesst` int(8) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+--
 -- AUTO_INCREMENT for table `data_pasien`
 --
 ALTER TABLE `data_pasien`
-  MODIFY `id_pasien` int(8) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id_pasien` int(8) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
